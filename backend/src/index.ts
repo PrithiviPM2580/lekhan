80cb54fab200cc99cdad643882ac72dbf12a6781
PrithiviPM2580 <prithivipemimagar>
2025-11-05 20:08:17 +0545

    feat: refactor server initialization to include database connection and graceful shutdown handling

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,23 @@
 import app from "app.js";
-import "dotenv/config";
+import {
+  connectToDatabase,
+  gracefullyShutdownDatabase,
+} from "config/database.config.js";
+import config from "config/envValidation.config.js";
+import logger from "lib/logger.lib.js";
 
-const PORT = process.env.PORT || 3000;
+const PORT = config.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+(async () => {
+  try {
+    await connectToDatabase();
+    app.listen(PORT, () => {
+      logger.info(`🚀 Server is running on http://localhost:${PORT}`);
+    });
+    process.on("SIGINT", () => gracefullyShutdownDatabase(app));
+    process.on("SIGTERM", () => gracefullyShutdownDatabase(app));
+  } catch (error) {
+    logger.error("❌Error starting the server", { error });
+    process.exit(1);
+  }
+})();
